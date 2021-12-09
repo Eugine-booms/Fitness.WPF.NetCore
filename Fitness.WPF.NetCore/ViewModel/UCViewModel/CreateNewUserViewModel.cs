aba4efb0ff18ca25dcff7debb54aa020e5f70734
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-09 17:56:29 +0300

    VM для нового пользователя и смены пользователя

---
+++ Fitness.WPF.NetCore/ViewModel/UCViewModel/CreateNewUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Boomsa.WPF.BaseLib.ViewModel.Base;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.WPF.NetCore.ViewModel.UCViewModel
+{
+    internal class CreateNewUserViewModel : ViewModelBase
+    {
+        private User user;
+
+        public CreateNewUserViewModel()
+        {
+            user = new User();
+        }
+        public void SetUserName(string Name)
+        {
+            user.Name = Name;
+        }
+
+    }
+}
---
+++ Fitness.WPF.NetCore/ViewModel/UCViewModel/CurentUserChangeViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Boomsa.WPF.BaseLib.ViewModel.Base;
+using Fitness.WPF.NetCore.Model;
+using Fitness.WPF.NetCore.View.UserControls;
+using Fitness.WPF.NetCore.View.Windows;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Fitness.WPF.NetCore.ViewModel.UCViewModel
+{
+    internal class CurentUserChangeViewModel : ViewModelBase
+    {
+
+        private ICollectionView _users;
+        public ICollectionView Users
+        {
+            get => _users;
+            set => Set(ref _users, value);
+        }
+
+
+        private string _textBoxLogin;
+        public string TextBoxLogin
+        {
+            get => _textBoxLogin;
+            set => Set(ref _textBoxLogin, value);
+        }
+        private User _curentUser;
+        public User CurentUser
+        {
+            get => _curentUser;
+            set => Set(ref _curentUser, value);
+
+
+        }
+        public CurentUserChangeViewModel()
+        {
+            Users = CollectionViewSource.GetDefaultView(UserManager.LoadUsers());
+            EnterUserCommand = new DelegateCommand(EnterUser, CanEnterUser);
+            NewUserCommand = new DelegateCommand(NewUser);
+        }
+
+        private bool CanEnterUser(object arg)
+        {
+            return (arg as User) != null;
+        }
+        private void EnterUser(object obg)
+        {
+            var newUserwindow = new MainWindow();
+            var newUserwindowViewModel = new MainWindowViewModel();
+            newUserwindow.ShowDialog();
+
+        }
+        private void NewUser(object obj)
+        {
+            var newUserwindow = new CreateNewUser();
+            var newUserwindowViewModel = new CreateNewUserViewModel();
+            newUserwindow.ShowDialog();
+        }
+
+        public ICommand EnterUserCommand { get; private set; }
+        public ICommand NewUserCommand { get; private set; }
+    }
+}
