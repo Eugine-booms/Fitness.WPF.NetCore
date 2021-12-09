414fc0f507e60cb387debfb06d3176b8e27044b3
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-09 19:22:18 +0300

    очистка

--- Fitness.WPF.NetCore/Services/Registrator.cs
+++ Fitness.WPF.NetCore/Services/Registrator.cs
@@ -3,18 +3,12 @@ using Boomsa.WPF.BaseLib.Services.Interfaces;
 
 using Microsoft.Extensions.DependencyInjection;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Fitness.WPF.NetCore.Services
 {
-  static internal  class Registrator
+    internal static class Registrator
     {
         internal static IServiceCollection RegisterServices(this IServiceCollection services) => services
             .AddTransient<IUserDialog, UserDialog>();
-           
+
     }
 }
