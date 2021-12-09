0c17a93814e651f03f5ce59ea2251b2a60613e05
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-09 18:04:17 +0300

    Простейшие интерфейсы

---
+++ Fitness.Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Fitness.Interfaces
+{
+    public interface IRepository<T> where T : class, IEntity, new()
+    {
+        IQueryable<T> Items { get; }
+        T Get(int id);
+        Task<T> GetAsync(int id, CancellationToken Cancel = default);
+        T Add(T items);
+        Task<T> AddAsync(T items, CancellationToken Cancel = default);
+        T Update(T items);
+        Task<T> UpdateAsync(T items, CancellationToken Cancel = default);
+        T Remove(int id);
+        Task<T> RemoveAsync(int id, CancellationToken Cancel = default);
+    }
+}
