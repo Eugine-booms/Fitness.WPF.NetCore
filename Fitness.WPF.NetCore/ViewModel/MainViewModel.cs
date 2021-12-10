364c3eb2076c6b4884ae6f61fa1191eaf220f7d2
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-10 11:45:50 +0300

    Заготовка команды смены пользователя

--- Fitness.WPF.NetCore/ViewModel/MainViewModel.cs
+++ Fitness.WPF.NetCore/ViewModel/MainViewModel.cs
@@ -1,9 +1,35 @@
 ﻿using Boomsa.WPF.BaseLib.ViewModel.Base;
+using Boomsa.WPF.BaseLib.Infrastructure.Command;
+using System.Windows.Input;
 
 namespace Fitness.WPF.NetCore.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
 
+        public MainViewModel()
+        {
+        }
+
+
+        #region Commands
+
+
+        #region Команда ChangeUser
+        private ICommand _ChangeUserCommand;
+        /// <summary>"Описание"</summary>
+        public ICommand ChangeUserCommand =>
+        _ChangeUserCommand ??=
+        new LambdaCommand(OnChangeUserCommandExecuted, CanChangeUserCommandExecute);
+        private void OnChangeUserCommandExecuted(object p)
+        {
+
+        }
+        private bool CanChangeUserCommandExecute(object p) => true;     
+        
+
+        #endregion
+
+        #endregion
     }
 }
