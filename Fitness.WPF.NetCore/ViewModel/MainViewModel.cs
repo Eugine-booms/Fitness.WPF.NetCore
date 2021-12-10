4cc97bcf13f68e56d1830d05d2017e6890fd647a
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-10 17:18:59 +0300

    получение юзеров , хз возможно откажусь от этого

--- Fitness.WPF.NetCore/ViewModel/MainViewModel.cs
+++ Fitness.WPF.NetCore/ViewModel/MainViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using Boomsa.WPF.BaseLib.ViewModel.Base;
 using Boomsa.WPF.BaseLib.Infrastructure.Command;
 using System.Windows.Input;
+using Fitness.DAL.Entities;
+using Fitness.DAL;
+using Fitness.Interfaces;
+using System.Windows.Controls;
+using Fitness.WPF.NetCore.View.Windows;
 
 namespace Fitness.WPF.NetCore.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-
-        public MainViewModel()
+        private readonly IRepository<User> dbUsers;
+        public MainViewModel(IRepository<User> _dbUsers)
         {
+            dbUsers = _dbUsers;
         }
 
 
@@ -17,13 +23,17 @@ namespace Fitness.WPF.NetCore.ViewModel
 
         #region Команда ChangeUser
         private ICommand _ChangeUserCommand;
+        
+
+
         /// <summary>"Описание"</summary>
         public ICommand ChangeUserCommand =>
         _ChangeUserCommand ??=
         new LambdaCommand(OnChangeUserCommandExecuted, CanChangeUserCommandExecute);
         private void OnChangeUserCommandExecuted(object p)
         {
-
+            var window = new CreateNewUserWindow();
+            window.Show();
         }
         private bool CanChangeUserCommandExecute(object p) => true;     
         
