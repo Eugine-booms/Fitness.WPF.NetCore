af04b325b4aa80d5de6b98c1fa3f725e5f1b40f1
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-10 17:18:26 +0300

    Добавление нового окна в локатор

--- Fitness.WPF.NetCore/ViewModel/Locator.cs
+++ Fitness.WPF.NetCore/ViewModel/Locator.cs
@@ -5,5 +5,7 @@ namespace Fitness.WPF.NetCore.ViewModel
     public class Locator
     {
         public MainViewModel GetMainVM => App.Services.GetRequiredService<MainViewModel>();
+        public CreateNewUserViewModel GetCreateuserVM => App.Services.GetRequiredService<CreateNewUserViewModel>();
+
     }
 }
