0e77251c9f5078d00e9099814bd83604adb5a21d
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-12 13:55:04 +0300

    Add FileLogger

---
+++ Fitness.WPF.NetCore/Infrastructure/Logger/FileLoggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Microsoft.Extensions.Logging
+{
+    class FileLoggerProvider : ILoggerProvider
+    {
+        private string path;
+        public FileLoggerProvider(string path)
+        {
+            this.path = path;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+            path = null;
+           
+        }
+    }
+}
