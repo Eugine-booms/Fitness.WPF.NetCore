d31df4914050b442f0b4bb5d5156d5a1625c02a4
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-14 15:59:59 +0300

    Сервис переключения фреймов

---
+++ Fitness.WPF.NetCore/Services/Interfaces/IFrameSwither.cs
@@ -0,0 +1,15 @@
+﻿using Fitness.WPF.NetCore.View.MainFrame;
+
+using System.Windows.Controls;
+
+namespace Fitness.WPF.NetCore.Services
+{
+    internal interface IFrameSwither
+    {
+        IFrameCanSwitch PageSwitcher { get; set; }
+
+        void AddPageToDictionary(Page page, string name);
+        void SwitchFrame(string name);
+       
+    }
+}
---
+++ Fitness.WPF.NetCore/Services/Interfaces/ISwitcher.cs
@@ -0,0 +1,15 @@
+﻿using Boomsa.WPF.BaseLib.ViewModel.Base;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.WPF.NetCore.Services.Interfaces
+{
+  public  interface ISwitcher
+    {
+        void Navigate(ViewModelBase model);
+    }
+}
