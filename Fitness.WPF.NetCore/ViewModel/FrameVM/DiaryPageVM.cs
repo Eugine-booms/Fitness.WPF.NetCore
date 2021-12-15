4f3a1af60eb1786b08c9ba7c4681581803c7d6ac
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-12-15 12:15:32 +0300

    Реализована функция отображения текущего дня

--- Fitness.WPF.NetCore/ViewModel/FrameVM/DiaryPageVM.cs
+++ Fitness.WPF.NetCore/ViewModel/FrameVM/DiaryPageVM.cs
@@ -1,4 +1,11 @@
-﻿using Boomsa.WPF.BaseLib.ViewModel.Base;
+﻿
+
+using Boomsa.WPF.BaseLib.ViewModel.Base;
+
+using Fitness.DAL.Entities;
+using Fitness.Interfaces;
+
+using Microsoft.Extensions.DependencyInjection;
 
 using System;
 using System.Collections.Generic;
@@ -10,5 +17,75 @@ namespace Fitness.WPF.NetCore.ViewModel.FrameVM
 {
     class DiaryPageVM : ViewModelBase
     {
+        private readonly IRepository<Day> _days;
+        private readonly MainViewModel _mainModel;
+        private User _user;
+
+
+        #region  DateTime SelectedDay Выбранный день, по умолчанию сегодня
+        ///<summary> Выбранный день, по умолчанию сегодня
+        private DateTime _SelectedDay =DateTime.Now;
+        ///<summary> Выбранный день, по умолчанию сегодня
+        public DateTime SelectedDay
+        {
+            get => _SelectedDay;
+            set
+            {
+                if (!Set(ref _SelectedDay, value, nameof(SelectedDay))) return;
+                CurrentDay = ConvertDayToString(value);
+            }
+        }
+        #endregion
+
+        #region  string CurrentDay Отображение текущего выбранного дня 
+        ///<summary> Отображение текущего выбранного дня 
+        private string _CurrentDay;
+        ///<summary> Отображение текущего выбранного дня 
+        public string CurrentDay
+        {
+            get => _CurrentDay;
+            set 
+            {
+                
+                Set(ref _CurrentDay, value, nameof(CurrentDay));
+            }
+        }
+
+        private string ConvertDayToString(DateTime selectedDay)
+        {
+            var nowDate = DateTime.Now.Date;
+            var difference = nowDate - selectedDay;
+            switch (difference.Days)
+            {
+                case 0:
+                    return "Сегодня";
+                case 1:
+                    return "Вчера";
+
+
+                case 2:
+                    return "Позавчера";
+                case -1:
+                    return "Завтра";
+                case -2:
+                    return "Послезавтра";
+                default:
+                    return difference.Days.ToString();
+            }      
+        }
+        #endregion
+
+
+        public DiaryPageVM(IRepository<Day> days)
+        {
+
+            _days = days;
+            // _user = PageSwitcherVM;
+        }
+        public DiaryPageVM()
+        {
+            if (!App.IsDesignTime)
+                throw new InvalidOperationException("Использование конструктора для дизайн мода");
+        }
     }
 }
